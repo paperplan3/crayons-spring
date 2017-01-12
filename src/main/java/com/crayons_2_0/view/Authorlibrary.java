30b3f3b53ea4b5f3fa3bb12de925359f2b022dd2
Natalia Krylova <n.k.krylova@gmail.com>
2017-01-12 14:40:10 +0100

    Removed a graph window.

--- src/main/java/com/crayons_2_0/view/Authorlibrary.java
+++ src/main/java/com/crayons_2_0/view/Authorlibrary.java
@@ -2,22 +2,14 @@ package com.crayons_2_0.view;
 
 import com.crayons_2_0.component.CourseEditor;
 import com.crayons_2_0.component.CourseEditor.CourseEditorListener;
-import com.crayons_2_0.component.GraphEditor;
 import com.crayons_2_0.mockup.autorenbereich;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.server.FontAwesome;
-import com.vaadin.server.Page;
-import com.vaadin.server.Sizeable.Unit;
-import com.vaadin.shared.Position;
-import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
@@ -108,21 +100,6 @@ public class Authorlibrary extends VerticalLayout implements View, CourseEditorL
         footer.setSpacing(true);
         footer.setComponentAlignment(CreateCourse, Alignment.TOP_CENTER);
         
-        /*GraphEditor graphEditor = new GraphEditor();
-        
-        Button editGraph = new Button("Edit graph");
-        editGraph.addStyleName(ValoTheme.BUTTON_PRIMARY);
-        editGraph.addClickListener(new ClickListener() {
-
-            @Override
-            public void buttonClick(ClickEvent event) {
-                UI.getCurrent().addWindow(graphEditor);
-            }
-            
-        });
-        footer.addComponent(editGraph);
-        footer.setSpacing(true);
-        footer.setComponentAlignment(editGraph, Alignment.TOP_CENTER);*/
         return footer;
     }
 
