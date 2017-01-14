e911a78658cd2246ed2b8a80005cbdaca8662d0b
juliusbuschweimar <JONG@192.168.0.11>
2017-01-14 18:54:29 +0100

    delete dummy authentication

--- src/main/java/com/crayons_2_0/view/login/LoginScreen.java
+++ src/main/java/com/crayons_2_0/view/login/LoginScreen.java
@@ -1,24 +1,12 @@
 package com.crayons_2_0.view.login;
-import java.io.Serializable;
 
-import com.crayons_2_0.dashboard.dummy.authentification.AccessControl;
 import com.vaadin.annotations.Theme;
-import com.vaadin.event.ShortcutAction;
-import com.vaadin.navigator.View;
-import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
-import com.vaadin.server.Page;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
-import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.PasswordField;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * UI content when the user is not logged in yet.
@@ -30,12 +18,6 @@ public class LoginScreen extends CssLayout {
      * 
      */
     private static final long serialVersionUID = 1L;
-    private TextField username;
-    private PasswordField password;
-    private Button login;
-    private Button forgotPassword;
-    private LoginListener loginListener;
-    private AccessControl accessControl;
     //AccessControl accessControl, LoginListener loginListener
     public LoginScreen() {
         //this.loginListener = loginListener;
@@ -67,51 +49,6 @@ public class LoginScreen extends CssLayout {
         addComponent(loginInformation);
     }
     
-    private Component buildLoginForm() {
-        FormLayout loginForm = new FormLayout();
-
-        loginForm.addStyleName("login-form");
-        loginForm.setSizeUndefined();
-        loginForm.setMargin(false);
-
-        loginForm.addComponent(username = new TextField("Username", "admin"));
-        username.setWidth(15, Unit.EM);
-        loginForm.addComponent(password = new PasswordField("Password"));
-        password.setWidth(15, Unit.EM);
-        password.setDescription("Write anything");
-        CssLayout buttons = new CssLayout();
-        buttons.setStyleName("buttons");
-        loginForm.addComponent(buttons);
-
-        buttons.addComponent(login = new Button("Login"));
-        login.setDisableOnClick(true);
-        login.addClickListener(new Button.ClickListener() {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                try {
-                    login();
-                } finally {
-                    login.setEnabled(true);
-                }
-            }
-        });
-        login.setClickShortcut(ShortcutAction.KeyCode.ENTER);
-        login.addStyleName(ValoTheme.BUTTON_FRIENDLY);
-        
-        buttons.addComponent(forgotPassword = new Button("Forgot password?"));
-        forgotPassword.addClickListener(new Button.ClickListener() {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                showNotification(new Notification("Hint: Try anything"));
-            }
-        });
-        forgotPassword.addStyleName(ValoTheme.BUTTON_LINK);
-        return loginForm;
-    }
     
 
     private CssLayout buildLoginInformation() {
@@ -125,27 +62,6 @@ public class LoginScreen extends CssLayout {
         return loginInformation;
     }
 
-    private void login() {
-        if (accessControl.signIn(username.getValue(), password.getValue())) {
-            loginListener.loginSuccessful();
-        } else {
-            showNotification(new Notification("Login failed",
-                    "Please check your username and password and try again.",
-                    Notification.Type.HUMANIZED_MESSAGE));
-            username.focus();
-        }
-    }
-
-    private void showNotification(Notification notification) {
-        // keep the notification visible a little while after moving the
-        // mouse, or until clicked
-        notification.setDelayMsec(2000);
-        notification.show(Page.getCurrent());
-    }
-
-    public interface LoginListener extends Serializable {
-        void loginSuccessful();
-    }
 
     
 }
