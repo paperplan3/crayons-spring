432dd62937433bfe0d27a7b8a9d8b554da282764
s_lschic <ufebn@student.kit.edu>
2017-01-16 17:17:40 +0100

    Add UserBibManager

--- src/main/java/com/crayons_2_0/view/UserlibraryView.java
+++ src/main/java/com/crayons_2_0/view/UserlibraryView.java
@@ -1,6 +1,8 @@
 package com.crayons_2_0.view;
 
+import com.crayons_2_0.controller.UserBibManager;
 import com.crayons_2_0.mockup.Benutzeransicht;
+import com.crayons_2_0.service.database.CourseService;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.FontAwesome;
@@ -9,17 +11,22 @@ import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.VerticalLayout;
 @SpringUI
-public class Userlibrary extends VerticalLayout implements View {
+public class UserlibraryView extends VerticalLayout implements View {
 
+	
     /**
      * 
      */
     private static final long serialVersionUID = 1L;
     public static final String VIEW_NAME = "Userlibrary";
 
-    public Userlibrary() {
+    
+    private TabSheet tabSheet;
+    
+    public UserlibraryView() {
 
         // you can add Vaadin components in predefined slots in the custom
         // layout
@@ -28,7 +35,16 @@ public class Userlibrary extends VerticalLayout implements View {
         setSizeFull();
         setStyleName("about-view");
         setMargin(true);
+        
+        // ------- View 1 ---------------
         addComponent(new Benutzeransicht());
+        
+        // ------- View 2 ---------------
+        /*
+        CourseService courseService = new CourseService();
+        this.tabSheet = UserBibManager.getCourseTabs(tabSheet);
+        addComponent(this.tabSheet);
+    	*/
     }
 
     @Override
