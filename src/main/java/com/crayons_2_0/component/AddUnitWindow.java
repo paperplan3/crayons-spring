332110ca494710042c51c50adf35b6dcb79ee8f5
Natalia Krylova <n.k.krylova@gmail.com>
2017-01-16 13:36:55 +0100

    Learning graph is a layout now and can be acsessed from an about view (later - from autorenbib)

--- src/main/java/com/crayons_2_0/component/AddUnitWindow.java
+++ src/main/java/com/crayons_2_0/component/AddUnitWindow.java
@@ -1,19 +1,15 @@
 package com.crayons_2_0.component;
 
-import com.vaadin.server.Page;
-import com.vaadin.shared.Position;
+import com.crayons_2_0.service.graph.AddNewUnitListener;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.themes.ValoTheme;
 
 @SuppressWarnings({ "serial" })
@@ -53,19 +49,7 @@ public final class AddUnitWindow extends Window {
 
         Button ok = new Button("Create");
         ok.addStyleName(ValoTheme.BUTTON_PRIMARY);
-        ok.addClickListener(new ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                close();
-                Notification success = new Notification(
-                        "Unit created successfully");
-                success.setDelayMsec(2000);
-                success.setStyleName("bar success small");
-                success.setPosition(Position.BOTTOM_CENTER);
-                success.show(Page.getCurrent());
-
-            }
-        });
+        ok.addClickListener(new AddNewUnitListener());
         ok.focus();
         footer.addComponent(ok);
         footer.setComponentAlignment(ok, Alignment.TOP_CENTER);
--- src/main/java/com/crayons_2_0/service/graph/DeleteUnitListener.java
+++ src/main/java/com/crayons_2_0/service/graph/DeleteUnitListener.java
@@ -1,14 +1,23 @@
 package com.crayons_2_0.service.graph;
 
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
 
 public class DeleteUnitListener  implements Button.ClickListener {
 
     @Override
     public void buttonClick(ClickEvent event) {
-        // TODO Auto-generated method stub
-        
+        for (Window window: UI.getCurrent().getWindows()) window.close();
+        Notification success = new Notification(
+                "Unit is deleted successfully");
+        success.setDelayMsec(2000);
+        success.setStyleName("bar success small");
+        success.setPosition(Position.BOTTOM_CENTER);
+        success.show(Page.getCurrent());
     }
-
 }
