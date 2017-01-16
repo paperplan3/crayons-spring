295e1756ef1ed9dd0a183101e4b1ed6b6c205d07
Natalia Krylova <n.k.krylova@gmail.com>
2017-01-16 10:59:50 +0100

    Graph models.

--- src/main/java/com/crayons_2_0/model/Course.java
+++ src/main/java/com/crayons_2_0/model/Course.java
@@ -1,18 +1,63 @@
 package com.crayons_2_0.model;
 
+import java.util.Date;
 import java.util.LinkedList;
 
 import org.springframework.security.core.userdetails.User;
 
+import com.crayons_2_0.model.graph.Graph;
+
 public class Course {
 	
 	private String title;
-	
 	private String description;
-	
+	private Date creationTime;
 	private LinkedList<User> authors;
+	//private LinkedList<Unit> Units;		// Struktur!!!!! 
+	private Graph graph;
 	
-	private LinkedList<Unit> Units;		// Struktur!!!!! 
+	public Course(String title, String description) {
+	    this.title = title;
+	    this.description = description;
+	}
 	
-
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public LinkedList<User> getAuthors() {
+        return authors;
+    }
+    
+    public void addAuthor(User author) {
+        this.authors.add(author);
+    }
+    
+    public void removeAuthor(User author) {
+        this.authors.remove(author);
+    }
+    
+    public Graph getGraph() {
+        return graph;
+    }
+    
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+    }
+    
+    public Date getCreationTime() {
+        return creationTime;
+    }
 }
---
+++ src/main/java/com/crayons_2_0/model/graph/Graph.java
@@ -0,0 +1,58 @@
+package com.crayons_2_0.model.graph;
+
+import com.crayons_2_0.model.graph.Unit.UnitType;
+
+public class Graph {
+    private final Unit startUnit; 
+    private final Unit endUnit;
+    private boolean unitsAvailable;
+    
+    // create a new graph when a new course is created
+    public Graph() {
+        this.unitsAvailable = false;
+        this.startUnit = new Unit(UnitType.START, unitsAvailable, this);
+        this.endUnit = new Unit(UnitType.END, unitsAvailable, this);
+    }
+    
+    // initialize a graph using the information from the databases
+    public Graph(long graphID) {
+        this.unitsAvailable = false;
+        this.startUnit = new Unit(UnitType.START, unitsAvailable, this);
+        this.endUnit = new Unit(UnitType.END, unitsAvailable, this);
+    }
+    
+    public Unit getStartUnit() {
+        return startUnit;
+    }
+
+    public Unit getEndUnit() {
+        return endUnit;
+    }
+
+    public boolean areUnitsAvailable() {
+        return unitsAvailable;
+    }
+
+    public void setUnitsAvailable(boolean unitsAvailable) {
+        this.unitsAvailable = unitsAvailable;
+    }
+    
+    // add a new unit and connect to the start
+    public boolean addUnit(UnitType unitType){
+        Unit newUnit = new Unit(unitType, unitsAvailable, this);
+        return true;
+    }
+    
+    public boolean deleteUnit(Unit learningUnit) {
+        return true;
+    }
+    
+    // remove an old connection if exist (alert?) and add a new one
+    public boolean addConnection(Unit from, Unit to) {
+        return true;
+    }
+    
+    public boolean removeConnection(Unit from, Unit to) {
+        return true;
+    }
+}
