4d0694852b842dad9105a62020779e59f940bbd2
uocxp <ali-akil@gmx.de>
2017-01-16 22:53:51 +0100

    package model.impExpPath for saving to a specific path or
    importing from 
    
    a path with its classes is created. Also Export Import classes in
    service package for handing the operations created too.

---
+++ src/main/java/com/crayons_2_0/service/Export.java
@@ -0,0 +1,27 @@
+package com.crayons_2_0.service;
+
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class Export implements Button.ClickListener{
+
+ 
+    
+    @Override
+    public void buttonClick(ClickEvent event) {      
+        for (Window window: UI.getCurrent().getWindows()) window.close();
+        Notification success = new Notification(
+                "file is exported successfully");
+        success.setDelayMsec(2000);
+        success.setStyleName("bar success small");
+        success.setPosition(Position.BOTTOM_CENTER);
+        success.show(Page.getCurrent());
+    }
+
+   
+}
