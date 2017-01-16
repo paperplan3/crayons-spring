07724bee2c065060324c55f5f8b2024a26c2eca4
Natalia Krylova <n.k.krylova@gmail.com>
2017-01-16 11:49:21 +0100

    Add listeners for the learning graph

---
+++ src/main/java/com/crayons_2_0/service/graph/AddNewUnitListener.java
@@ -0,0 +1,14 @@
+package com.crayons_2_0.service.graph;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class AddNewUnitListener implements Button.ClickListener {
+
+    @Override
+    public void buttonClick(ClickEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ src/main/java/com/crayons_2_0/service/graph/DeleteConnectionListener.java
@@ -0,0 +1,15 @@
+package com.crayons_2_0.service.graph;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class DeleteConnectionListener  implements Button.ClickListener {
+
+    @Override
+    public void buttonClick(ClickEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
+
