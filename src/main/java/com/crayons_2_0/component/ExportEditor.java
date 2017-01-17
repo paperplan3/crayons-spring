1e06175550255be8f16d1c6947d2e6e503d642c9
uocxp <ali-akil@gmx.de>
2017-01-17 18:24:55 +0100

    Import- and  ExportEditors updated

--- src/main/java/com/crayons_2_0/component/ExportEditor.java
+++ src/main/java/com/crayons_2_0/component/ExportEditor.java
@@ -1,7 +1,6 @@
 package com.crayons_2_0.component;
 
-import java.util.ResourceBundle; 
-
+import java.util.ResourceBundle;
 
 import com.vaadin.server.Page;
 import com.vaadin.shared.Position;
@@ -10,20 +9,36 @@ import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
- 
-public class ExportEditor implements Button.ClickListener{
- 
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.ui.Component;
 
-    Button export = new Button("Export");
-   
-    @Override
-    public void buttonClick(ClickEvent event) {      
-        for (Window window: UI.getCurrent().getWindows()) window.close();
-        Notification success = new Notification(
-                "file is exported successfully");
-        success.setDelayMsec(2000);
-        success.setStyleName("bar success small");
-        success.setPosition(Position.BOTTOM_CENTER);
-        success.show(Page.getCurrent());
+public class ExportEditor   {
+
+    public ExportEditor() {
+        // TODO Auto-generated constructor stub
     }
+
+    public Component ExportButton(){
+        
+        Button export = new Button("Export");
+        export.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        export.addClickListener(new ClickListener() {
+            
+            @Override
+            public void buttonClick(ClickEvent event) {
+                for (Window window : UI.getCurrent().getWindows())
+                    window.close();
+                Notification success = new Notification("file is exported successfully");
+                success.setDelayMsec(2000);
+                success.setStyleName("bar success small");
+                success.setPosition(Position.BOTTOM_CENTER);
+                success.show(Page.getCurrent());
+                
+            }
+        });
+        return export;
+        
+    }
+    
 }
