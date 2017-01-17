484abc1d8f7a7ec467fe7601bac14b056affc979
uocxp <ali-akil@gmx.de>
2017-01-17 13:39:19 +0100

    move  exportToPath and ImportFromPath
    from model.impExpPath to service.impExpPath

--- src/main/java/com/crayons_2_0/service/Export.java
+++ src/main/java/com/crayons_2_0/service/Export.java
@@ -8,20 +8,10 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
 
-public class Export implements Button.ClickListener{
+public class Export  {
 
  
     
-    @Override
-    public void buttonClick(ClickEvent event) {      
-        for (Window window: UI.getCurrent().getWindows()) window.close();
-        Notification success = new Notification(
-                "file is exported successfully");
-        success.setDelayMsec(2000);
-        success.setStyleName("bar success small");
-        success.setPosition(Position.BOTTOM_CENTER);
-        success.show(Page.getCurrent());
-    }
-
+ 
    
 }
--- src/main/java/com/crayons_2_0/service/Import.java
+++ src/main/java/com/crayons_2_0/service/Import.java
@@ -1,26 +1,8 @@
 package com.crayons_2_0.service;
 
-import com.vaadin.server.Page;
-import com.vaadin.shared.Position;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.Window;
-import com.vaadin.ui.Button.ClickEvent;
 
-public class Import implements Button.ClickListener{
+public class Import  {
 
-    @Override
-    public void buttonClick(ClickEvent event) {
-        for (Window window: UI.getCurrent().getWindows()) window.close();
-        Notification success = new Notification(
-                "file is imported successfully");
-        success.setDelayMsec(2000);
-        success.setStyleName("bar success small");
-        success.setPosition(Position.BOTTOM_CENTER);
-        success.show(Page.getCurrent());
-        
-    }
-
+   
     
 }
