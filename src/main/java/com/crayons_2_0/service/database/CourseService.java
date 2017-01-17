c1e2fac120715d4f0359a94eeb9f156abc646347
Levin Schickle - ufebn <lschickle93@googlemail.com>
2017-01-17 16:16:08 +0100

    Fix head Errors

--- src/main/java/com/crayons_2_0/service/database/CourseService.java
+++ src/main/java/com/crayons_2_0/service/database/CourseService.java
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-package com.crayons_2_0.service.database;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-
-import com.crayons_2_0.model.Course;
-
-public class CourseService {
-	
-	@Autowired
-    private CourseDAO courseDAO;
-	
-	public List<Course> findAll() {
-	    List<Course> res = courseDAO.findAll();
-	    return res;
-	}
-	
-	public Course getCourseByTitle(String courseTitle) {
-		//TODO
-		return null;
-	}
-	
-	public List<Course> getAllCursesOfUser() {
-		//TODO
-		return null;
-	}
-	
-	public boolean createNewCourse(String title, User author) {
-		
-		// Wenn Kurs kreiert werden kann, erstelle kurs in DB
-		
-		// Checke - Kurs Existiert?
-		for (Course tmpCourse : courseDAO.findAll()) {
-			if (tmpCourse.getTitle().equals(title)) {
-				return false;
-			}
-		}
-		
-		Course newCourse = new Course(title, "No description");
-		courseDAO.save(newCourse);
-	
-		return true;
-	}
-	
-
-}
-=======
 package com.crayons_2_0.service.database;
 
 import java.util.List;
@@ -93,8 +43,7 @@ public class CourseService {
 		Course newCourse = new Course(title,author);
 	
 		return true;
-	}
-	
+	}	
 
 }
->>>>>>> refs/remotes/origin/master
+
