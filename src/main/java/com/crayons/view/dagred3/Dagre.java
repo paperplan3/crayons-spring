b04683bd9fe6f95afbc2d073758c653db6907f6e
jubusch <julius.busch@gmail.com>
2017-01-31 21:38:24 +0100

    added EdgeSequence Conversion for Javascript Connector

--- src/main/java/com/crayons/view/dagred3/Dagre.java
+++ src/main/java/com/crayons/view/dagred3/Dagre.java
@@ -11,8 +11,8 @@ import com.vaadin.ui.AbstractJavaScriptComponent;
 
 @JavaScript({"style.css","http://cpettitt.github.io/project/dagre-d3/v0.3.0/dagre-d3.js","http://d3js.org/d3.v3.min.js","com_crayons_view_dagred3_Dagre.js" })
 public class Dagre extends AbstractJavaScriptComponent {
-    public void setNodes(final ArrayList<String> set) {
-        getState().setUnitNodes(set);
+    public void setGraph(final ArrayList<String> nodes,final ArrayList<String> edges) {
+        getState().setGraph(nodes,edges);
     }
 
     @Override
--- src/main/java/com/crayons/view/dagred3/com_crayons_view_dagred3_Dagre.js
+++ src/main/java/com/crayons/view/dagred3/com_crayons_view_dagred3_Dagre.js
@@ -2,85 +2,73 @@
  * 
  */
 window.com_crayons_view_dagred3_Dagre = function() {
-	var g = new dagreD3.graphlib.Graph()
-	  .setGraph({})
-	  .setDefaultEdgeLabel(function() { return {}; });
-/*
-	// Here we"re setting nodeclass, which is used by our custom drawNodes function
-	// below.
-	g.setNode(0,  { label: "Start",     class: "t" });
-	g.setNode(1,  { label: "Lineare Algebra",         class: "t" });
-	g.setNode(2,  { label: "Höhere Mathematik",        class: "t" });
-	g.setNode(3,  { label: "Programmieren",        class: "t" });
-	g.setNode(4,  { label: "Praxis in der Softwareentwicklung",      class: "t" });
-	g.setNode(5,  { label: "Betriebssystheme",        class: "t" });
-	g.setNode(6,  { label: "Rechnerorganisation",       class: "t" });
-	g.setNode(7,  { label: "Algorithmen",        class: "t" });
-	g.setNode(8,  { label: "Sicherheit",        class: "t" });
-	g.setNode(9,  { label: "Grundbegriffe der Informatik",        class: "t" });
-	g.setNode(10, { label: "Numerik",        class: "t" });
-	g.setNode(11, { label: "Datenbanken",        class: "t" });
-	g.setNode(12, { label: "Digitaltechnik",   class: "t" });
-	g.setNode(13, { label: "Programmierparadigmen",         class: "t" });
-	g.setNode(14, { label: "Softwaretechnik",  class: "t" });
-	g.setNode(15, { label: "Ende",  class: "t" });
-
-
-	g.nodes().forEach(function(v) {
-	  var node = g.node(v);
-	  // Round the corners of the nodes
-	  node.rx = node.ry = 5;
-	});
-
-	// Set up edges, no special attributes.
-	g.setEdge(3, 4);
-	g.setEdge(2, 3);
-	g.setEdge(1, 2);
-	g.setEdge(6, 7);
-	g.setEdge(5, 6);
-	g.setEdge(9, 10);
-	g.setEdge(8, 9);
-	g.setEdge(11,12);
-	g.setEdge(8, 11);
-	g.setEdge(5, 8);
-	g.setEdge(1, 5);
-	g.setEdge(13,14);
-	g.setEdge(1, 13);
-	g.setEdge(0, 1);
-	g.setEdge(4, 15);
-	g.setEdge(7, 15);
-	g.setEdge(10, 15);
-	g.setEdge(12, 15);
-	g.setEdge(14, 15);
-*/	
+	var g = new dagreD3.graphlib.Graph({}).setGraph({});
+	/*
+	g.graph().transition = function transition(selection) { // transition with
+		// duration 1000ms
+		return selection.transition().duration(1000);
+	};*/
+	/*
+	 * // Here we"re setting nodeclass, which is used by our custom drawNodes
+	 * function // below. g.setNode(0, { label: "Start", class: "t" });
+	 * g.setNode(1, { label: "Lineare Algebra", class: "t" }); g.setNode(2, {
+	 * label: "Höhere Mathematik", class: "t" }); g.setNode(3, { label:
+	 * "Programmieren", class: "t" }); g.setNode(4, { label: "Praxis in der
+	 * Softwareentwicklung", class: "t" }); g.setNode(5, { label:
+	 * "Betriebssystheme", class: "t" }); g.setNode(6, { label:
+	 * "Rechnerorganisation", class: "t" }); g.setNode(7, { label:
+	 * "Algorithmen", class: "t" }); g.setNode(8, { label: "Sicherheit", class:
+	 * "t" }); g.setNode(9, { label: "Grundbegriffe der Informatik", class: "t"
+	 * }); g.setNode(10, { label: "Numerik", class: "t" }); g.setNode(11, {
+	 * label: "Datenbanken", class: "t" }); g.setNode(12, { label:
+	 * "Digitaltechnik", class: "t" }); g.setNode(13, { label:
+	 * "Programmierparadigmen", class: "t" }); g.setNode(14, { label:
+	 * "Softwaretechnik", class: "t" }); g.setNode(15, { label: "Ende", class:
+	 * "t" });
+	 * 
+	 * 
+	 * g.nodes().forEach(function(v) { var node = g.node(v); // Round the
+	 * corners of the nodes node.rx = node.ry = 5; }); // Set up edges, no
+	 * special attributes. g.setEdge(3, 4); g.setEdge(2, 3); g.setEdge(1, 2);
+	 * g.setEdge(6, 7); g.setEdge(5, 6); g.setEdge(9, 10); g.setEdge(8, 9);
+	 * g.setEdge(11,12); g.setEdge(8, 11); g.setEdge(5, 8); g.setEdge(1, 5);
+	 * g.setEdge(13,14); g.setEdge(1, 13); g.setEdge(0, 1); g.setEdge(4, 15);
+	 * g.setEdge(7, 15); g.setEdge(10, 15); g.setEdge(12, 15); g.setEdge(14,
+	 * 15);
+	 */
 	var Nodes = this.getState().unitNodes;
-	//create graphNodes
-	for(count=0;count < Nodes.length;count++){
+	
+	// create graphNodes
+	for (count = 0; count < Nodes.length; count++) {
 		var tmp = Nodes[count];
-		g.setNode(tmp,{label:tmp, class: "t"})
+		g.setNode(tmp, {label : tmp,class : "t"})
 	}
 	g.nodes().forEach(function(v) {
-		  var node = g.node(v);
-		  // Round the corners of the nodes
-		  node.rx = node.ry = 5;
-		});
-	/*
-	for(count=0;count < myArr.length;count++){
-		g.setEdge(myArr[count].getUnitNodeTitle(),myArr[count].getUnitNodeTitle())
-	}
-	*/
+		var node = g.node(v);
+		// Round the corners of the nodes
+		node.rx = node.ry = 5;
+	});
 	
+	var Edges = this.getState().edges;
+	for (var count = 0; count < Edges.length; count += 2) {
+		var tmpEdgeBegin = Edges[count];
+		var tmpEdgeEnd = Edges[count + 1];
+
+		g.setEdge(tmpEdgeBegin, tmpEdgeEnd, {})
+
+	}
+
 	/*
-	 this.onStateChange = function() {
-	        var coords = this.getState().coords;
-	        d3.selectAll("circle").transition().attr("cx", parseInt(coords[0]));
-	 }*/
+	 * this.onStateChange = function() { var coords = this.getState().coords;
+	 * d3.selectAll("circle").transition().attr("cx", parseInt(coords[0])); }
+	 */
 	// Create the renderer
 	var render = new dagreD3.render();
 
 	// Set up an SVG group so that we can translate the final graph.
-	var svg = d3.select(this.getElement()).append("svg:svg").attr("width", 1000).attr("height", 500),
-	    svgGroup = svg.append("g");
+	var svg = d3.select(this.getElement()).append("svg:svg")
+			.attr("width", 1000).attr("height", 500), svgGroup = svg
+			.append("g");
 
 	// Run the renderer. This is what draws the final graph.
 	render(d3.select("svg g"), g);
@@ -89,7 +77,5 @@ window.com_crayons_view_dagred3_Dagre = function() {
 	var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
 	svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
 	svg.attr("height", g.graph().height + 40);
-	
-	
 
 }
